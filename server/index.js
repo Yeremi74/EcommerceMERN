41c6e97251b5e0ad20ac1a030419805de8163436
Yeremi74 <yyeremi15@gmail.com>
2024-04-12 13:53:28 -0400

    fixing cors

--- server/index.js
+++ server/index.js
@@ -3,5 +3,5 @@ import { PORT } from './config.js';
 import app from './app.js';
 
 connectDB();
-app.listen(PORT);
-console.log(`server is running in port`, PORT);
+app.listen(3001);
+console.log(`server is running in port`, 3001);
