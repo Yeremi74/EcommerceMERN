0c1f46fdedada8e26d834ebdf647604788efd1f8
Yeremi74 <yyeremi15@gmail.com>
2024-04-12 16:18:53 -0400

    fix url backend

--- client/src/api/colleciones.js
+++ client/src/api/colleciones.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 const backend = import.meta.env.VITE_BACKEND_URL;
 export const getCollectionRequest = async () =>
-  await axios.get(`${backend}api/collections`);
+  await axios.get(`${VITE_BACKEND_URL || backend}api/collections`);
