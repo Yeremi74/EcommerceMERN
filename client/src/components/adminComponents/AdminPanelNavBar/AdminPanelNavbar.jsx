1fe7cd5749a3f908341a7c17a59fab156f96c12d
Yeremi74 <yyeremi15@gmail.com>
2024-04-13 02:27:37 -0400

    testing

--- client/src/components/adminComponents/AdminPanelNavBar/AdminPanelNavbar.jsx
+++ client/src/components/adminComponents/AdminPanelNavBar/AdminPanelNavbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const AdminPanelNavbar = () => {
   return (
-    <div className='flex flex-col bg-gray-700 h-screen w-44 fixed text-white text-3xl text-center justify-around items-center text-red-600'>
+    <div className='flex flex-col bg-gray-700 h-screen w-44 fixed text-3xl text-center justify-around items-center text-red-600'>
       <Link
         className='hover:bg-gray-400 transition-all w-fit p-2 rounded-md'
         to='/'
