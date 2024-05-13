3fd458bb469a9317b95e1b60cdee80e8c45bfdfc
Yeremi74 <yyeremi15@gmail.com>
2024-05-13 00:50:59 -0400

    testing back

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -25,8 +25,8 @@ export const register = async (req, res) => {
     const token = await createAccessToken({ id: userSaved._id });
 
     res.cookie('token', token, {
-      sameSite: 'none', // Establece SameSite como None
-      secure: true, // Debe establecerse como true si SameSite es None y estás usando HTTPS
+      // sameSite: 'none', // Establece SameSite como None
+      // secure: true, // Debe establecerse como true si SameSite es None y estás usando HTTPS
     });
 
     res.json({
@@ -58,8 +58,8 @@ export const login = async (req, res) => {
     const token = await createAccessToken({ id: userFound._id });
 
     res.cookie('token', token, {
-      sameSite: 'none', // Establece SameSite como None
-      secure: true, // Debe establecerse como true si SameSite es None y estás usando HTTPS
+      // sameSite: 'none', // Establece SameSite como None
+      // secure: true, // Debe establecerse como true si SameSite es None y estás usando HTTPS
     });
 
     res.json({
